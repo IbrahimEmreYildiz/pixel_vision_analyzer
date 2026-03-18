ecdd0fecb68c52c923d9715008c635d4bdf774cc
İbrahim Emre Yıldız <iemreis803@gmail.com>
2026-03-18 16:59:03 +0300

    Son hatalar düzeltildi

--- analyzer.py
+++ analyzer.py
@@ -3,36 +3,31 @@ import numpy as np
 class Analyze:
 
 
-    def __init__(self):
-        pass
-
-
-    def brightnessCalculator(image_matris):
-        brightness = np.mean(image_matris)
+    def __init__(self,image):
+        self.image=image
         
+
+    def brightnessCalculator(self):
+        brightness = np.mean(self.image)
+
         return brightness
 
 
-    def classifyLight(light):
-        
-        if light >=128:
-            classify="Lighter"
-        else:
-            classify="Darker"
-            
-        return classify
+    def classifyLight(self):
+        return self.brightnessCalculator()>100
 
 
-    def edgeDetector(image_matris):
-        
-        result = np.diff(image_matris)
-        
-        if np.any(result>=30):
-            alarm="Edge Detected"
-        else:
-            alarm="Flat Surface"
+    def edgeDetector(self):
+        edges=[]
+        rows,cols= self.image.shape
 
-        return alarm
+        for i in range(rows-1):
+            for j in range(cols-1):
+                diff=abs(int(self.image[i][j])- int(self.image[i][j+1]))
+                if diff>30:
+                    edges.append((i,j))
+        return edges
+
 
 
 
