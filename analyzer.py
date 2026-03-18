51b7c1fb2649f5ec3ebad41bac7b66e913a0217a
İbrahim Emre Yıldız <iemreis803@gmail.com>
2026-03-18 16:39:07 +0300

    OOP yapısı analiz dosyasına eklendi.

--- analyzer.py
+++ analyzer.py
@@ -1,32 +1,38 @@
 import numpy as np
 
+class Analyze:
 
 
-def brightnessCalculator(image_matris):
-    brightness = np.mean(image_matris)
-    return brightness
+    def __init__(self):
+        pass
 
 
-def classifyLight(light):
-    
-    if light >=128:
-        classify="Lighter"
-    else:
-        classify="Darker"
+    def brightnessCalculator(image_matris):
+        brightness = np.mean(image_matris)
         
-    return classify
+        return brightness
 
 
-def edgeDetector(image_matris):
-    
-    result = np.diff(image_matris)
-    
-    if np.any(result>=30):
-        alarm="Edge Detected"
-    else:
-        alarm="Flat Surface"
+    def classifyLight(light):
         
-    return alarm
+        if light >=128:
+            classify="Lighter"
+        else:
+            classify="Darker"
+            
+        return classify
+
+
+    def edgeDetector(image_matris):
+        
+        result = np.diff(image_matris)
+        
+        if np.any(result>=30):
+            alarm="Edge Detected"
+        else:
+            alarm="Flat Surface"
+
+        return alarm
 
 
 
