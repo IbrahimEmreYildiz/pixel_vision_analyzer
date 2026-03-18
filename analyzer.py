62eeb965e9ed873ed2ccf0e4294221fb071d3535
İbrahim Emre Yıldız <iemreis803@gmail.com>
2026-03-18 16:34:35 +0300

    Analiz fonksiyonları yazıldı

---
+++ analyzer.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+
+def brightnessCalculator(image_matris):
+    brightness = np.mean(image_matris)
+    return brightness
+
+
+def classifyLight(light):
+    
+    if light >=128:
+        classify="Lighter"
+    else:
+        classify="Darker"
+        
+    return classify
+
+
+def edgeDetector(image_matris):
+    
+    result = np.diff(image_matris)
+    
+    if np.any(result>=30):
+        alarm="Edge Detected"
+    else:
+        alarm="Flat Surface"
+        
+    return alarm
+
+
+
